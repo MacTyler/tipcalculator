b706006f8ac9dbe15958fc6d5b5dc1b1182fba00
mactyler <mactyler@fb.com>
2013-10-01 14:35:23 -0700

    Tip Calculator iOS App

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Mac Tyler on 9/29/13.
+//  Copyright (c) 2013 Mac Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
